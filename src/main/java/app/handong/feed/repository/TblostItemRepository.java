a575589109424c79d08e5204ec08ac1ab5d5c9bf
callendev20 <callendev20@gmail.com>
2024-11-01 11:41:17 +0900

    :white_check_mark: Add TblostItemRepository

---
+++ src/main/java/app/handong/feed/repository/TblostItemRepository.java
@@ -0,0 +1,10 @@
+package app.handong.feed.repository;
+
+import app.handong.feed.domain.TblostItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface TblostItemRepository extends JpaRepository<TblostItem, String> {
+}
