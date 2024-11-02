6054d5f71dcaf7fd2fbcb2d6f1bf9927126db610
callendev20 <callendev20@gmail.com>
2024-11-02 13:46:32 +0900

    :sparkles: Implement TblostItemService.getLostItemDetail

--- src/main/java/app/handong/feed/service/TblostItemService.java
+++ src/main/java/app/handong/feed/service/TblostItemService.java
@@ -1,5 +1,6 @@
 package app.handong.feed.service;
 
+import app.handong.feed.dto.DefaultDto;
 import app.handong.feed.dto.TblostItemDto;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -10,5 +11,5 @@ import java.util.List;
 public interface TblostItemService {
 
     TblostItemDto.CreateResDto createLostItem(TblostItemDto.CreateReqDto createReqDto, List<MultipartFile> files, String reqUserId);
-
+    TblostItemDto.DetailResDto getLostItemDetail(DefaultDto.IdReqDto idReqDto);
 }
--- src/main/java/app/handong/feed/service/impl/TblostItemServiceImpl.java
+++ src/main/java/app/handong/feed/service/impl/TblostItemServiceImpl.java
@@ -1,9 +1,11 @@
 package app.handong.feed.service.impl;
 
 import app.handong.feed.domain.TblostItem;
+import app.handong.feed.dto.DefaultDto;
 import app.handong.feed.dto.TblostItemDto;
+import app.handong.feed.mapper.TblostItemMapper;
 import app.handong.feed.repository.TblostItemRepository;
-import app.handong.feed.service.FirebaseStorageService;
+import app.handong.feed.service.FirebaseService;
 import app.handong.feed.service.TblostItemService;
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
@@ -15,11 +17,13 @@ import java.util.List;
 public class TblostItemServiceImpl implements TblostItemService {
 
     private final TblostItemRepository tblostRepository;
-    private final FirebaseStorageService firebaseStorageService;
+    private final TblostItemMapper tblostItemMapper;
+    private final FirebaseService firebaseService;
 
-    public TblostItemServiceImpl(TblostItemRepository tblostRepository, FirebaseStorageService firebaseStorageService) {
+    public TblostItemServiceImpl(TblostItemRepository tblostRepository, TblostItemMapper tblostItemMapper, FirebaseService firebaseService) {
         this.tblostRepository = tblostRepository;
-        this.firebaseStorageService = firebaseStorageService;
+        this.tblostItemMapper = tblostItemMapper;
+        this.firebaseService = firebaseService;
     }
 
     @Transactional
@@ -27,10 +31,26 @@ public class TblostItemServiceImpl implements TblostItemService {
         TblostItem tblostItem = TblostItem.of(reqUserId, createReqDto.getLostPersonName(), createReqDto.getContent());
         TblostItem savedLostItem = tblostRepository.save(tblostItem);
 
-        List<String> fileUrls = firebaseStorageService.uploadFiles(files, "LostItem", savedLostItem.getId());
+        List<String> fileUrls = firebaseService.uploadFiles(files, "LostItem", savedLostItem.getId());
 
         return TblostItemDto.CreateResDto.builder()
                 .id(savedLostItem.getId())
+                .fileUrls(fileUrls)
+                .build();
+    }
+
+    @Override
+    public TblostItemDto.DetailResDto getLostItemDetail(DefaultDto.IdReqDto idReqDto) {
+        TblostItemDto.DetailServDto detail = tblostItemMapper.getLostItemDetailById(idReqDto.getId());
+        List<String> fileUrls = detail.getFileNames().stream()
+                .map(firebaseService::generateFileUrl)
+                .toList();
+
+        return TblostItemDto.DetailResDto.builder()
+                .id(detail.getId())
+                .lostPersonName(detail.getLostPersonName())
+                .content(detail.getContent())
+                .createdAt(detail.getCreatedAt())
                 .fileUrls(fileUrls)
                 .build();
     }
