7db476714f84da2e94a9059c07b874b548e83379
callendev20 <callendev20@gmail.com>
2024-11-03 01:56:02 +0900

    :white_check_mark: Add findByTblostId - TblostItemFileRepository

--- src/main/java/app/handong/feed/repository/TblostItemFileRepository.java
+++ src/main/java/app/handong/feed/repository/TblostItemFileRepository.java
@@ -4,7 +4,10 @@ import app.handong.feed.domain.TblostItemFile;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 
 @Repository
 public interface TblostItemFileRepository extends JpaRepository<TblostItemFile, String> {
+    List<TblostItemFile> findByTblostId(String tblostId);
 }
