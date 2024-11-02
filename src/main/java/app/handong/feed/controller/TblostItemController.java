14c59afb10077d30a27e1c599b8e2ec4400315bf
callendev20 <callendev20@gmail.com>
2024-11-02 13:46:54 +0900

    :white_check_mark: Add getLostItem API

--- src/main/java/app/handong/feed/controller/TblostItemController.java
+++ src/main/java/app/handong/feed/controller/TblostItemController.java
@@ -1,5 +1,6 @@
 package app.handong.feed.controller;
 
+import app.handong.feed.dto.DefaultDto;
 import app.handong.feed.dto.TblostItemDto;
 import app.handong.feed.service.TblostItemService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -45,5 +46,18 @@ public class TblostItemController {
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
+    @Operation(summary = "분실물 조회",
+            description = "분실물 ID로 분실물 정보를 조회합니다. <br />"
+                    + "@param Long lostId <br />"
+                    + "@return HttpStatus.OK(200) ResponseEntity\\<TblostDto.DetailResDto\\> <br />"
+                    + "@exception 404 Not Found (분실물을 찾을 수 없는 경우)"
+    )
+    @GetMapping(value = "/{lostId}")
+    public ResponseEntity<TblostItemDto.DetailResDto> getLostItem(
+            @PathVariable("lostId") String lostId) {
+        TblostItemDto.DetailResDto response = tblostService.getLostItemDetail(DefaultDto.IdReqDto.builder().id(lostId).build());
+
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
 
 }
