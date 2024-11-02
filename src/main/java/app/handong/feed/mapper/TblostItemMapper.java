8ea81224821b64614236c17df397c6965617cbe3
callendev20 <callendev20@gmail.com>
2024-11-02 15:45:08 +0900

    :sparkles: Implement TblostItemMapper.getAllLostItems

--- src/main/java/app/handong/feed/mapper/TblostItemMapper.java
+++ src/main/java/app/handong/feed/mapper/TblostItemMapper.java
@@ -4,7 +4,10 @@ import app.handong.feed.dto.TblostItemDto;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
+
 @Mapper
 public interface TblostItemMapper {
     TblostItemDto.DetailServDto getLostItemDetailById(@Param("itemId") String itemId);
+    List<TblostItemDto.AllServDto> getAllLostItems();
 }
