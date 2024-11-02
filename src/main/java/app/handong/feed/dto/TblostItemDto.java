64be0151d2648ffe9bb53bf116508af00350babb
callendev20 <callendev20@gmail.com>
2024-11-02 22:15:20 +0900

    🔀 Merge AllServDto into DetailServDto since mapper logic changes - TblostItemDto

--- src/main/java/app/handong/feed/dto/TblostItemDto.java
+++ src/main/java/app/handong/feed/dto/TblostItemDto.java
@@ -43,27 +43,13 @@ public class TblostItemDto {
         private List<String> fileUrls;
     }
 
-    @Schema(description = "분실물 상세 조회 서비스 DTO")
+    @Schema(description = "분실물 조회 서비스 DTO")
     @Getter
     @Setter
     @Builder
     @AllArgsConstructor
     @NoArgsConstructor
     public static class DetailServDto {
-        private String id;
-        private String lostPersonName;
-        private String content;
-        private LocalDateTime createdAt;
-        private List<String> fileNames;
-    }
-
-    @Schema(description = "분실물 전체 조회 서비스 DTO")
-    @Getter
-    @Setter
-    @Builder
-    @AllArgsConstructor
-    @NoArgsConstructor
-    public static class AllServDto {
         private String id;
         private String lostPersonName;
         private String content;
