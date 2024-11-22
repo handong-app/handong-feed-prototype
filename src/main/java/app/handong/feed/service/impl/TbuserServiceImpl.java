e8bf87027075d7d1cdfe2ef586be4c8ade849289
Sangdeveloper7 <leesanghyun9757@gmail.com>
2024-11-22 20:19:19 +0900

    Change format

--- src/main/java/app/handong/feed/service/impl/TbuserServiceImpl.java
+++ src/main/java/app/handong/feed/service/impl/TbuserServiceImpl.java
@@ -24,13 +24,11 @@ public class TbuserServiceImpl implements TbuserService {
     @Autowired
     private final TbuserRepository tbuserRepository;
     private final GoogleAuthService googleAuthService;
-
-
+    
     public TbuserServiceImpl(TbuserRepository tbuserRepository, GoogleAuthService googleAuthService) {
         this.tbuserRepository = tbuserRepository;
         this.googleAuthService = googleAuthService;
     }
-
 
     @Override
     public TbuserDto.DetailResDto detail(DefaultDto.DetailReqDto param) {
@@ -53,7 +51,6 @@ public class TbuserServiceImpl implements TbuserService {
         return returnMap;
     }
 
-
     @Override
     public TbuserDto.CreateResDto access(String param) throws Exception {
         TokenFactory tokenFactory = new TokenFactory();
@@ -62,11 +59,8 @@ public class TbuserServiceImpl implements TbuserService {
         return null;
     }
 
-
     @Override
     public TbuserDto.CreateResDto loginWithGoogle(String credential) {
-
-
         String[] out = googleAuthService.verifyGoogleToken(credential);
         String email = out[0];
         String name = out[1];
@@ -77,23 +71,18 @@ public class TbuserServiceImpl implements TbuserService {
         System.out.println("email string is " + email);
         System.out.println("email name is " + name);
 
-        // 유저 객체 생성
-
-
         boolean checkHandongPeople = false;
         // 유저가 @handong.ac.kr 이메일이 아니면 예외 처리
-        if (email != null){
-            if(email.endsWith("@handong.ac.kr")){
+        if (email != null) {
+            if (email.endsWith("@handong.ac.kr")) {
                 checkHandongPeople = true;
-            }
-            else if(email.endsWith("@handong.edu")) {
+            } else if (email.endsWith("@handong.edu")) {
                 checkHandongPeople = true;
             }
         }
-
-        if(!checkHandongPeople){
+        if (!checkHandongPeople) {
             throw new NoAuthorizationException("Not Handong User");
-        }else{
+        } else {
             Tbuser tbuser = tbuserRepository.findByEmail(email);
 
             if (tbuser != null) {
@@ -105,30 +94,8 @@ public class TbuserServiceImpl implements TbuserService {
                 // 유저가 없는 경우 신규 가입
             }
             tbuserRepository.save(tbuser);
-
             return TbuserDto.CreateResDto.builder().uid(tbuser.getId()).email(email).build();
         }
-
-//        Tbuser tbuser = tbuserRepository.findByEmail(email);
-//
-//        if (tbuser != null) {
-//            // 유저가 있으면 이메일 출력
-//            System.out.println("user " + tbuser.getEmail());
-//            tbuser.setLast_login_time(now);
-//        } else {
-//            tbuser = Tbuser.of(email, uuid, name, now, now, now);
-//            // 유저가 없는 경우 신규 가입
-//        }
-//        tbuserRepository.save(tbuser);
-//
-////        Map<String, Object> params = new HashMap<>();
-////        params.put("email", email);
-////        params.put("uuid", uuid);
-////        params.put("name", name);
-////
-////        Map<String, Object> createResult = create(params);
-//
-//        return TbuserDto.CreateResDto.builder().uid(tbuser.getId()).email(email).build();
     }
 
 
