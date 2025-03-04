9d0e49c2fcd6749bee94df2a4c784579ce635738
junglesub <junglesubmarine@gmail.com>
2025-03-04 19:44:25 +0900

    feat(client): update client alerts.

--- src/main/front/src/components/MainDisplay.jsx
+++ src/main/front/src/components/MainDisplay.jsx
@@ -19,23 +19,9 @@ function MainDisplay({ children, noCount = false }) {
         component="main"
         sx={{ flexGrow: 1, px: 1.5, pb: 6, maxWidth: 700, mx: "auto" }}
       >
-        <Alert severity="error" sx={{ borderRadius: 0, my: 1 }}>
-          <AlertTitle>
-            많은 방문자로 인해 이미지가 불러와지지 않습니다{" "}
-            <a
-              style={{ color: "black" }}
-              href="https://board.handong.app/feed/num/32"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              (#32)
-            </a>
-          </AlertTitle>
-          현재 개발팀이 문제를 해결 중에 있습니다. 불편을 드려 죄송합니다.
-        </Alert>
         <Alert severity="warning" sx={{ borderRadius: 0, my: 1 }}>
           <AlertTitle>
-            새로운 피드 추가가 <b>지연</b>되는 오류 발생{" "}
+            현재 새로운 피드가 <b>늦게</b> 올라가는 오류가 있습니다.
             <a
               style={{ color: "black" }}
               href="https://board.handong.app/feed/num/31"
@@ -45,8 +31,8 @@ function MainDisplay({ children, noCount = false }) {
               (#31)
             </a>
           </AlertTitle>
-          현재 개발팀이 문제를 해결 중이나, 방학 기간으로 인해 시간이 다소 걸릴
-          수 있습니다.
+          개발팀에서 하루에 한 번 수동으로 추가하고 있으니 조금만 기다려 주세요.
+          이용에 불편을 드려 죄송합니다.
         </Alert>
 
         {children}
