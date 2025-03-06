f233de84b5792f27353d0dcbb72c735101c256c6
junglesub <junglesubmarine@gmail.com>
2025-03-06 14:32:38 +0900

    feat(client): add 'mark all as read' functionality with confirmation modal

--- src/main/front/src/components/MainDisplay.jsx
+++ src/main/front/src/components/MainDisplay.jsx
@@ -19,6 +19,21 @@ function MainDisplay({ children, noCount = false }) {
         component="main"
         sx={{ flexGrow: 1, px: 1.5, pb: 6, maxWidth: 700, mx: "auto" }}
       >
+        <Alert severity="info" sx={{ borderRadius: 0, my: 1 }}>
+          <AlertTitle>
+            우측 하단 '모두 읽음 처리' 기능 추가되었습니다{" "}
+            <a
+              style={{ color: "black" }}
+              href="https://board.handong.app/feed/num/33"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              (#33)
+            </a>
+          </AlertTitle>
+          우측 하단에 알림을 한 번에 모두 읽음으로 변경할 수 있는 기능이
+          추가되었습니다.
+        </Alert>
         <Alert severity="warning" sx={{ borderRadius: 0, my: 1 }}>
           <AlertTitle>
             현재 새로운 피드가 <b>늦게</b> 올라가는 오류가 있습니다.
--- src/main/front/src/pages/MainFeed.jsx
+++ src/main/front/src/pages/MainFeed.jsx
@@ -7,14 +7,28 @@ import { useFeedCount } from "../hooks/useFeed";
 import { useFetchBe } from "../tools/api";
 import { useSetRecoilState } from "recoil";
 import { feedCountAtom } from "../recoil/feedAtom";
-import { Card, CardContent, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  Card,
+  CardContent,
+  Fab,
+  Modal,
+  Tooltip,
+  Typography,
+} from "@mui/material";
+import MarkEmailReadOutlinedIcon from "@mui/icons-material/MarkEmailReadOutlined";
+import { useNavigate } from "react-router-dom";
 
 function MainFeed() {
   const fetch = useFetchBe();
+  const navigate = useNavigate();
   const setFeedCount = useSetRecoilState(feedCountAtom);
   const [allFeeds, hasMore, loadData] = useLoadData({ type: "unseen" });
   const [feedNumber] = useFeedCount();
   const [doingBulkDelete, setDoingBulkDelete] = useState(false);
+
+  const [readAllModal, setReadAllModal] = useState(0);
 
   useEffect(() => {
     if (doingBulkDelete || hasMore || feedNumber < 1) return;
@@ -30,6 +44,76 @@ function MainFeed() {
 
   return (
     <MainDisplay>
+      <Modal open={readAllModal != 0}>
+        <Card
+          sx={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            width: 400,
+            p: 2,
+          }}
+        >
+          <CardContent>
+            <Typography variant="h6" component="div" gutterBottom>
+              모든 피드를 읽음 처리하시겠습니까?
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              이 행동은 되돌릴 수 없습니다.
+            </Typography>
+            <Box sx={{ display: "flex", justifyContent: "flex-end", mt: 2 }}>
+              <Button
+                onClick={() => setReadAllModal(0)}
+                sx={{ mr: 1 }}
+                disabled={readAllModal === 2}
+              >
+                취소
+              </Button>
+              <Button
+                variant="contained"
+                color="error"
+                loading={readAllModal === 2}
+                onClick={() => {
+                  setReadAllModal(2);
+                  fetch("/feeduser/readall", "POST", {
+                    lastSentAt: allFeeds[0].sentAtEpoch,
+                  })
+                    .then(() => {
+                      console.log("reloading current page");
+                      scroll(0, 0);
+                      navigate(0);
+                    })
+                    .catch((e) => {
+                      alert(e.message);
+                      setReadAllModal(1);
+                    });
+                }}
+              >
+                읽음처리
+              </Button>
+            </Box>
+          </CardContent>
+        </Card>
+      </Modal>
+      <Tooltip title="모두 읽음 처리">
+        <Fab
+          color="primary"
+          aria-label="add"
+          disabled={allFeeds.length === 0}
+          sx={{
+            position: "fixed",
+            bottom: {
+              xs: 70,
+              sm: 20,
+            },
+            right: 20,
+          }}
+          onClick={() => setReadAllModal(1)}
+        >
+          <MarkEmailReadOutlinedIcon />
+        </Fab>
+      </Tooltip>
       {!hasMore && allFeeds.length === 0 && (
         <Card sx={{ my: 2 }}>
           <CardContent>
